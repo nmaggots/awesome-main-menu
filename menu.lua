66c70866466afc782ded8f76d2d97952c34b3c57
Children Maggot <nmaggots@gmail.com>
2018-02-05 23:02:55 +0900

    Create menu.lua

---
+++ menu.lua
@@ -0,0 +1,143 @@
+-- automatically generated file. Do not edit (see /usr/share/doc/menu/html)
+
+local awesome = awesome
+
+Debian_menu = {}
+
+Debian_menu["Debian_Applications_Accessories"] = {
+	{"Bluetooth Transfer","bluetooth-sendto","/usr/share/pixmaps/nate/bluetooth.svg"},
+	{"Screenshot","mate-screenshot --interactive"},
+	{"Catfish File Search","/usr/bin/catfish","/usr/share/pixmaps/nate/catfish.svg"},
+	{"Character Map","gucharmap","/usr/share/pixmaps/nate/fontforge.svg"},
+	{"Pluma","pluma ","/usr/share/pixmaps/nate/notecase.svg"},
+	{"Vim","vim ","/usr/share/pixmaps/nate/vim.svg"},
+	{"wxHexEditor","wxHexEditor","/usr/share/pixmaps/wxHexEditor.png"},
+	{"Zim Desktop Wiki","zim ","/usr/share/pixmaps/nate/zim.svg"},
+	{"Calculator","gnome-calculator","/usr/share/pixmaps/nate/gnome-calc.svg"},
+	{"Gip IP Address Calculator","/usr/bin/gip ","/usr/share/pixmaps/nate/calculator.svg"},
+	{"Leafpad","leafpad ","/usr/share/pixmaps/nate/leafpad.svg"},
+	{"Password & Keys","/usr/bin/seahorse","/usr/share/pixmaps/nate/seahorse.svg"},
+	{"VirtualBox","/usr/bin/virtualbox","/usr/share/pixmaps/nate/virtualbox.svg"},
+	{"Main Menu","/usr/bin/menulibre","/usr/share/pixmaps/nate/menulibre.svg"},
+	{"Caja Root","su-to-root -X -c caja","/usr/share/pixmaps/nate/caja-actions.svg"},
+}
+Debian_menu["Debian_Applications_Graphics"] = {
+	{"gtk-recordmydesktop","/usr/bin/gtk-recordmydesktop","/usr/share/pixmaps/nate/gtk-recordmydesktop.svg"},
+	{"Inkscape","/usr/bin/inkscape","/usr/share/pixmaps/nate/inkscape.svg"},
+	{"X Window Snapshot","xwd | xwud","/usr/share/pixmaps/nate/x.svg"},
+}
+Debian_menu["Debian_Applications_Internet"] = {
+	{"Angry IP Scanner","/usr/bin/ipscan","/usr/share/pixmaps/nate/ipscan.svg"},
+	{"Driftnet","driftnet","/usr/share/pixmaps/nate/terminal.svg"},
+	{"Ettercap","ettercap-pkexec -G","/usr/share/pixmaps/ettercap.svg"},
+	{"Firefox ESR Web Browser","/usr/lib/firefox-esr/firefox-esr ","/usr/share/pixmaps/nate/firefox.svg"},
+	{"Google Chrome","/usr/bin/google-chrome-stable","/usr/share/pixmaps/nate/chrome.svg"},
+	{"Kismet","kismet","/usr/share/pixmaps/nate/terminal.svg"},
+	{"QupZilla","qupzilla ","/usr/share/pixmaps/qupzilla.png"},
+	{"Remmina Remote Desktop Client","remmina","/usr/share/pixmaps/nate/remmina.svg"},
+	{"TeamViewer 12","/opt/teamviewer/tv_bin/script/teamviewer","/usr/share/pixmaps/nate/teamviewer.svg"},
+	{"Telegram Desktop","/opt/telegram/Telegram","/usr/share/pixmaps/nate/telegram.svg"},
+	{"TorChat","/usr/bin/torchat","/usr/share/pixmaps/nate/torchat.svg"},
+	{"w3af","/usr/share/w3af/w3af_gui","/usr/share/pixmaps/w3af.xpm"},
+	{"Wicd Network Manager","wicd-gtk","/usr/share/pixmaps/nate/wicd.svg"},
+	{"Wireshark","wireshark ","/usr/share/pixmaps/nate/wireshark.svg"},
+	{"XHydra","xhydra ","/usr/share/pixmaps/xhydra.xpm"},
+	{"Zenmap","su-to-root -X -c zenmap ","/usr/share/pixmaps/nate/zenmap.svg"},
+	{"Zoom","zoom","/usr/share/pixmaps/application-x-zoom.png"}
+}
+Debian_menu["Debian_Applications_Programming"] = {
+	{"GHex","ghex ","/usr/share/pixmaps/nate/ghex.svg"},
+	{"ipython","ipython","/usr/share/pixmaps/nate/ipython.svg"},
+	{"Meld Diff Viewer","meld ","/usr/share/pixmaps/nate/meld.svg"},
+	{"MonoDevelop","monodevelop ","/usr/share/pixmaps/nate/monodevelop.svg"},
+	{"Python (v3.5)","/usr/bin/python3.5","/usr/share/pixmaps/nate/python.svg"},
+	{"Pycharm Editor","/opt/pycharm/bin/pycharm.sh ","/opt/pycharm/bin/pycharm.png"},
+	{"Sublime Text","/opt/sublime_text/sublime_text ","/usr/share/pixmaps/nate/sublime.svg"},
+	{"GDB", "/usr/bin/gdb","/usr/share/pixmaps/nate/login.svg"},
+
+}
+Debian_menu["Debian_Applications_Office"] = {
+	{"WPS Presentation","/usr/bin/wpp ","/usr/share/pixmaps/nate/libreoffice3-impress.svg"},
+	{"WPS Spreadsheets","/usr/bin/et ","/usr/share/pixmaps/nate/libreoffice4.2-calc.svg"},
+	{"WPS Writer","/usr/bin/wps ","/usr/share/pixmaps/nate/libreoffice3-writer.svg"},
+}
+Debian_menu["Debian_Applications_Sound"] = {
+	{"PulseAudio Volume Control","/usr/bin/pavucontrol","/usr/share/pixmaps/nate/pavucontrol.svg"},
+	{"RecordMyDesktop","gtk-recordmydesktop","/usr/share/pixmaps/nate/gtk-recordmydesktop.svg"},
+	{"VLC media player","/usr/bin/vlc","/usr/share/pixmaps/nate/vlc.svg"},
+}
+
+Debian_menu["Debian_Applications_System_Preferences"] = {
+	{"Additional Drivers","/usr/bin/software-properties-gtk","/usr/share/pixmaps/nate/driver-manager.svg"},
+	{"Bluetooth Manager","blueman-manager","/usr/share/pixmaps/nate/blueman.svg"},
+	{"Customize Look and Feel","lxappearance","/usr/share/pixmaps/nate/"},
+	{"Desktop Preferences","pcmanfm","/usr/share/pixmaps/nate/"},
+	{"Openbox Configuration Manager","obconf ","/usr/share/pixmaps/nate/pcmanfm.svg"},
+	{"Screensaver","xscreensaver-demo","/usr/share/pixmaps/nate/screensaver.svg"},
+	{"Theme Configuration","gtk-theme-config","/usr/share/pixmaps/nate/gtk-theme-config.svg"},
+	{"Tint2 panel settings","tint2conf","/usr/share/pixmaps/nate/panel.svg"},
+}
+
+Debian_menu["Debian_Applications_System_Administration"] = {
+	{"GParted Partition Editor","gparted-pkexec","/usr/share/pixmaps/nate/gparted.svg"},
+	{"LightDM GTK+ Greeter settings","lightdm-gtk-greeter-settings-pkexec","/usr/share/pixmaps/nate/lightdm-gtk-greeter-settings.svg"},
+	{"Network","network-admin","/usr/share/pixmaps/nate/network-wired.svg"},
+	{"NTFS Configuration Tool","/usr/sbin/ntfs-config-root","/usr/share/pixmaps/nate/ntfs-config.svg"},
+	{"Shared Folders","shares-admin","/usr/share/pixmaps/nate/folder-template.svg"},
+	{"Software Updater","/usr/bin/update-manager","/usr/share/pixmaps/nate/software.svg"},
+	{"Startup Disk Creator","usb-creator-gtk","/usr/share/pixmaps/nate/usb-creator-gtk.svg"},
+	{"Time and Date","time-admin","/usr/share/pixmaps/nate/time-admin.svg"},
+	{"Users and Groups","users-admin","/usr/share/pixmaps/nate/user.svg"},
+}
+
+Debian_menu["Debian_Applications_Terminal_Emulators"] = {
+	{"LXTerminal","/usr/bin/lxterminal","/usr/share/pixmaps/lxterminal.xpm"},
+	{"Rxvt-Unicode","urxvt","/usr/share/pixmaps/urxvt.xpm"},
+	{"XTerm","xterm","/usr/share/pixmaps/xterm-color_32x32.xpm"},
+	{"Rxvt-Unicode (Black, Xft)","urxvt -fn \"xft:Mono\" -rv","/usr/share/pixmaps/urxvt.xpm"},
+	{"X-Terminal as root (GKsu)","/usr/bin/gksu -u root /usr/bin/x-terminal-emulator","/usr/share/pixmaps/gksu-debian.xpm"},
+	{"XTerm (Unicode)","uxterm","/usr/share/pixmaps/xterm-color_32x32.xpm"},
+}
+Debian_menu["Debian_Applications_Wine"] = {
+	{"Wine","winecfg","/usr/share/pixmaps/nate/winecfg.svg"},
+	{"Winetricks","winetricks","/usr/share/pixmaps/nate/winetricks.svg"},
+}
+Debian_menu["Debian_Applications_Tools"] = {
+	{ "Administration", Debian_menu["Debian_Applications_Tools_Administration"],"/usr/share/pixmaps/nate/gtk-preferences.svg" },
+	{ "Preferences", Debian_menu["Debian_Applications_Tools_Preferences"],"/usr/share/pixmaps/nate/gtk-theme-config.svg" },
+	{ "BleachBit (as root)","su-to-root -X -c bleachbit","/usr/share/pixmaps/nate/bleachbit.svg"},
+	{ "File Manager PCManFM","pcmanfm","/usr/share/pixmaps/nate/pcmanfm.svg"},
+	{ "GDebi Package Installer","gdebi-gtk ","/usr/share/pixmaps/nate/gdebi.svg"},
+	{ "Gigolo","gigolo","/usr/share/pixmaps/nate/gigolo.svg"},
+	{ "Guymager","gksudo guymager","/usr/share/pixmaps/guymager_128.png"},
+	{ "Ophcrack","ophcrack","/usr/share/pixmaps/ophcrack.xpm"},
+	{ "Reactivate HP LaserJet 1018/1020 after reloading paper","/usr/share/foo2zjs/hplj10xx_gui.tcl","/usr/share/pixmaps/nate/printer.svg"},
+	{ "Software Install","gnome-software ","/usr/share/pixmaps/nate/gnome-software.svg"},
+	{ "Ubuntu Customization Kit","uck-gui","/usr/share/pixmaps/uck.png"},
+	
+}
+
+Debian_menu["Penetrations_Applications"] = {
+	{"Information Gathering","xman"},
+}
+
+Debian_menu["Debian_Applications"] = {
+	{ "Accessories", Debian_menu["Debian_Applications_Accessories"],"/usr/share/pixmaps/nate/accessories-thesaurus.svg" },
+	{ "Graphics", Debian_menu["Debian_Applications_Graphics"],"/usr/share/pixmaps/nate/package_graphics.svg" },
+	{ "Internet", Debian_menu["Debian_Applications_Internet"],"/usr/share/pixmaps/nate/internet.svg" },
+	{ "Programming", Debian_menu["Debian_Applications_Programming"],"/usr/share/pixmaps/nate/lugaru.svg" },
+	{ "Office", Debian_menu["Debian_Applications_Office"],"/usr/share/pixmaps/nate/libreoffice3-writer.svg" },
+	{ "Sound", Debian_menu["Debian_Applications_Sound"],"/usr/share/pixmaps/nate/minitunes.svg" },
+	{ "Terminal Emulators", Debian_menu["Debian_Applications_Terminal_Emulators"],"/usr/share/pixmaps/nate/terminal.svg" },
+	{ "System", Debian_menu["Debian_Applications_Tools"],"/usr/share/pixmaps/nate/gkdebconf.svg" },
+	{ "Wine", Debian_menu["Debian_Applications_Wine"],"/usr/share/pixmaps/nate/wine.svg" },
+
+}
+
+Debian_menu["Debian"] = {
+	{ "Applications", Debian_menu["Debian_Applications"],"/usr/share/awesome/themes/default/powerarrow/icons/awesome.png" },
+	{ "Penetrations", Debian_menu["Penetrations_Applications"] },
+}
+
+debian = { menu = { Debian_menu = Debian_menu } }
+return debian
